c221696058b228d0663eaa39ccb709fbcf44f287
AnuchaNO21 <152251593+AnuchaNO21@users.noreply.github.com>
2024-07-31 15:44:09 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,10 @@
+public class TestFor {
+    
+        public static void main(String[] args) {
+            int multiplier = 2;
+            for (int i = 1; i <= 12; i++){
+                System.out.println(multiplier+" x " + i + " = "+(multiplier * i));
+            }
+        }
+    
+}
