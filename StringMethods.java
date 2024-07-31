ed59b66aca4ab50b26431d4556e20d3a87360da7
AnuchaNO21 <152251593+AnuchaNO21@users.noreply.github.com>
2024-07-31 15:44:06 +0700

    Update StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,20 @@
+public class StringMethods {
+    
+        public static void main(String[] args) {
+            String message = "Java is Great Fun";
+      
+        String upper = message.toUpperCase();
+        System.out.println(upper);
+    
+        String lower = message.toLowerCase();
+        System.out.println(lower);
+    
+        int stringSize = message.length();
+        System.out.println(stringSize);
+    
+        char lenter = message.charAt(3);
+        System.out.println(lenter);
+        }
+    
+    
+}
