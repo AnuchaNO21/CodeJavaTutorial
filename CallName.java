9e65d5812982902fb9d4aa98072ba51e50408112
AnuchaNO21 <152251593+AnuchaNO21@users.noreply.github.com>
2024-08-07 11:37:25 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,12 @@
+public class CallName {
+    public static void printName(String name) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        // เรียกใช้งานเมธอด
+        printName("Anucha ...");
+    }
+}
